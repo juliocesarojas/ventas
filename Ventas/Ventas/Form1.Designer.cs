f86fb9d1ce375bc658ee916d48148746fe110db5
Julio Rojas <Julio Rojas@JULIO-PC>
2021-09-16 19:44:48 -0600

    Segundo intento, sincronizando. Programa Frase Motivadora

--- Ventas/Ventas/Form1.Designer.cs
+++ Ventas/Ventas/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(79, 101);
+            this.button1.Location = new System.Drawing.Point(82, 55);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 49);
             this.button1.TabIndex = 0;
